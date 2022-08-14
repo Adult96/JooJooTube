52a922521ce2ef41fd4d5300d6394b9d1b4cead1
Adult96 <djxor007@gmail.com>
2022-08-14 16:32:50 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './app.css';
+import VideoList from './components/vidoe_list/video_list';
 
 function App() {
-  return <h1>hello</h1>;
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA4jfMuXNN1wszcF0lnumWJDxKFC7SuNg0',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideo(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={video} />;
 }
 
 export default App;
